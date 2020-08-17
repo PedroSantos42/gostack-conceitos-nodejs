bc15390d91c14432bd4c1ce9f2b2b07f5a7e1a45
pedro.santos <pedroabs.dev42@gmail.com>
2020-08-17 20:21:22 -0300

    challenge completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    let { title } = request.body;
+
+    const results = title
+        ? repositories.filter(repo => repo.title.includes(title))
+        : repositories;
+
+    return response.status(200).json(results);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    repo = {
+        id: uuid(),
+        ...request.body,
+        likes: 0
+    };
+
+    repositories.push(repo);
+
+    return response.status(201).send({ url, title, techs, likes } = repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    let repoAux = request.body;
+    let { id } = request.params;
+
+    let index = repositories.findIndex(repo => repo.id == id);
+
+    if (index < 0)
+        return response.status(400).json({ message: 'Repository does not exist' });
+
+    let { likes } = repoAux
+
+    if (likes)
+        return response.status(400).json({ likes: repositories[index].likes });
+
+    repositories[index] = {
+        ...repositories[index],
+        ...repoAux
+    };
+    return response.status(200).json({ url, title, techs } = repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    let { id } = request.params;
+    let index = repositories.findIndex(repo => repo.id == id);
+    if (index >= 0) {
+        repositories.splice(repositories.findIndex(repo => repo.id == id), 1);
+        return response.status(204).send();
+    } else {
+        return response.status(400).json({ message: 'Repository does not exist' });
+    }
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    let { id } = request.params;
+    let index = repositories.findIndex(repo => repo.id == id);
+
+    if (index >= 0) {
+        repositories[index] = {
+            ...repositories[index],
+            likes: repositories[index].likes + 1
+        };
+        return response.status(200).json({ likes: repositories[index].likes });
+    } else {
+        return response.status(400).json({ message: 'Repository does not exist' });
+    }
 });
 
 module.exports = app;
